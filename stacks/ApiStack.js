5ef7e4e872da6472b3f6f7d82a1a88ef76a0a013
mathewgries <mathewgries@gmail.com>
2021-10-08 17:15:20 -0400

    Added frontend

---
+++ stacks/ApiStack.js
@@ -0,0 +1,28 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class ApiStack extends sst.Stack {
+  api;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const { table } = props;
+
+    this.api = new sst.Api(this, "Api", {
+      defaultAuthorizationType: "AWS_IAM",
+      defaultFunctionProps: {
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+      cors: true,
+      routes: {},
+    });
+
+    this.api.attachPermissions([table]);
+
+    this.addOutputs({
+      ApiEndpoint: this.api.url,
+    });
+  }
+}
--- stacks/index.js
+++ stacks/index.js
@@ -1,12 +1,26 @@
-import MyStack from "./MyStack";
+import AuthStack from "./AuthStack";
+import ApiStack from "./ApiStack";
+import DynamoDbStack from "./DynamoDbStack";
+import FileUploadStack from "./FileUploadStack";
+import FrontendStack from "./FrontendStack";
 
 export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs12.x"
+  const dbStack = new DynamoDbStack(app, "table");
+
+  const fileUploadStack = new FileUploadStack(app, "bucket");
+
+  const apiStack = new ApiStack(app, "api", {
+    table: dbStack.table,
   });
 
-  new MyStack(app, "my-stack");
+  const authStack = new AuthStack(app, "auth", {
+    api: apiStack.api,
+    bucket: fileUploadStack.bucket,
+  });
 
-  // Add more stacks
+  new FrontendStack(app, "frontend", {
+    api: apiStack.api,
+    auth: authStack.auth,
+    bucket: fileUploadStack.bucket,
+  });
 }
