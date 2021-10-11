29432d1b96765c884eb4ee89ec922517fc98d43e
mathewgries <mathewgries@gmail.com>
2021-10-10 21:04:55 -0400

    DNS very much sucks balls3

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -9,8 +9,6 @@ export default class ApiStack extends sst.Stack {
 
     const { table } = props;
     const { stage } = this.node.root;
-    const certArn =
-      "arn:aws:acm:us-east-1:025747659854:certificate/73f5ae64-fdbf-4c43-914e-9c9cca2bd196";
 
     this.api = new sst.Api(this, "Api", {
       customDomain: HostedZone.fromLookUp(this, "mathewGriesDOTCOMZone", {
