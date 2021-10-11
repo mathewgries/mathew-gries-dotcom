2a067cd456c510244e4e5494e05b3e23a117b234
mathewgries <mathewgries@gmail.com>
2021-10-10 23:50:05 -0400

    DNS very much sucks balls8

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -1,5 +1,4 @@
 import * as sst from "@serverless-stack/resources";
-import { HostedZone } from "@aws-cdk/aws-route53";
 
 export default class ApiStack extends sst.Stack {
   api;
@@ -8,20 +7,8 @@ export default class ApiStack extends sst.Stack {
     super(scope, id, props);
 
     const { table } = props;
-    const { stage } = this.node.root;
 
     this.api = new sst.Api(this, "Api", {
-      customDomain: {
-        domainName: "mathewgries.com",
-        hostedZone: HostedZone.fromHostedZoneAttributes(this, "MyZone", {
-          hostedZoneId:
-            stage === "prod" ? "Z0446746E7NKHGM6KRL7" : "Z075300910GQYU7TM9XRM",
-          zoneName:
-            stage === "prod"
-              ? "prod.api.mathewgries.com"
-              : "dev.api.mathewgries.com",
-        }),
-      },
       defaultAuthorizationType: "AWS_IAM",
       defaultFunctionProps: {
         environment: {
@@ -35,7 +22,7 @@ export default class ApiStack extends sst.Stack {
     this.api.attachPermissions([table]);
 
     this.addOutputs({
-      ApiEndpoint: this.api.customDomainUrl || this.api.url,
+      ApiEndpoint: this.api.url,
     });
   }
 }
