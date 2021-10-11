5c6b65407fb2b1ef62aee6fa40a8823e2cd66a56
mathewgries <mathewgries@gmail.com>
2021-10-10 21:02:22 -0400

    DNS very much sucks balls2

--- stacks/FrontendStack.js
+++ stacks/FrontendStack.js
@@ -1,14 +1,10 @@
 import * as sst from "@serverless-stack/resources";
-import { HostedZone } from "@aws-cdk/aws-route53";
-import { Certificate } from "@aws-cdk/aws-certificatemanager";
 
 export default class FrontendStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
     const { api, auth, bucket } = props;
-    const certArn =
-      "arn:aws:acm:us-east-1:025747659854:certificate/73f5ae64-fdbf-4c43-914e-9c9cca2bd196";
 
     // Define our React app
     const site = new sst.ReactStaticSite(this, "ReactSite", {
